f5d0fe2f375ecf9fdfc8483b3f2245d483b0b639
xvw <xavier.vdw@gmail.com>
2017-04-13 16:01:52 +0200

    Initialize new version

--- lib/mizur.ex
+++ lib/mizur.ex
@@ -26,6 +26,7 @@ defmodule Mizur do
       import Mizur
       @base nil
       @operators %{}
+      @metrics []
       @before_compile Mizur
     end
   end
@@ -34,7 +35,7 @@ defmodule Mizur do
   defmacro __before_compile__(_env) do
     quote do
       def system_metric do
-        @operators
+        @metrics
       end
     end
   end
@@ -64,6 +65,71 @@ defmodule Mizur do
       end
     end
   end
+
+  @doc false
+  defmacro define_basis(name) do 
+    quote do
+      case @base do 
+        nil ->
+          def unquote(name)() do 
+            {__MODULE__, unquote(name), [
+              fn(x) -> x * 1.0 end,
+              fn(x) -> x * 1.0 end
+            ]}
+          end
+
+          def unquote(name)(value) do 
+            {apply(__MODULE__, unquote(name), []), value * 1.0}
+          end
+          
+          @base unquote(name)
+          @metrics [ unquote(name) | @metrics ]
+        _ -> 
+          log = "Basis is already defined: #{@base}"
+          raise RuntimeError, message: log
+      end
+    end
+  end
+
+  def fail_at(nl, msg) do 
+    module_line = "#{__MODULE__}:#{nl}"
+    raise RuntimeError, message: "line #{module_line} #{msg}"
+  end
+  
+
+  @doc false 
+  defmacro define_type(nl, name, expr) do 
+    quote do 
+      cond do 
+        Enum.member?(@metrics, unquote(name)) -> 
+          fail_at(unquote(nl), "#{unquote(name)} already exists")
+        true -> 
+          @metrics [ unquote(name) | @metrics ]
+          0
+      end  
+    end
+  end
+  
+
+  @doc false
+  defmacro v_type(value) do 
+
+    IO.inspect value
+
+    case value do 
+
+      value when is_atom(value) -> 
+        quote do: define_basis(unquote(value))
+
+      {:=, [line: nl], [value, expr]} ->
+        quote do: define_type(unquote(nl), unquote(value), unquote(expr))
+
+      {_, [line: nl], _} -> fail_at(nl, "is not parsable")
+        
+    end
+    
+  end
+  
 
   @doc false
   defmacro type(value) do
--- test/mizur_test.exs
+++ test/mizur_test.exs
@@ -17,127 +17,13 @@ defmodule MizurTest do
     type :dollar = :euro * 1.06665
   end
 
-  
-  test "for unwraping" do 
-    input = Length.cm(12)
-    assert (Mizur.unwrap input) == 12.0
+  defmodule Test do 
+    use Mizur 
+    v_type :celsius
+    v_type :farenheit = :celsius * 1.8 + 32.0
   end
 
-  test "infix usage of from/2" do
-    import Mizur
-    elt = (Length.m(1) ~> Length.cm) 
-    |> unwrap
-    assert elt == 100.0
-  end
+  IO.inspect Test.celsius(12)
 
-  test "new syntax" do 
-    a = Length.m(1)
-    b = Mizur.from(a, to: Length.mm)
-    assert (Mizur.unwrap b) == 1000.0
-  end
-  
-
-  test "for difftyped data" do 
-    k = Money.euro(12)
-    module = Money
-    other_module = Length
-    assert_raise RuntimeError, "#{module} is not compatible with #{other_module}", 
-    fn -> _ = Mizur.from(k, to: Length.cm) end
-  end
-
-  test "for conversion" do 
-    input = Length.cm(350)
-    to_m  = Mizur.from input, to: Length.m
-    assert Mizur.unwrap(to_m) == 3.5
-  end
-
-  test "for Mapping" do 
-    input = Length.cm(12)
-    |> Mizur.map(fn(x) -> x + 10 end)
-    |> Mizur.unwrap
-    assert input == 22.0
-  end
-
-  test "for map2" do 
-    a = Length.dm(12)
-    b = Length.dm(34)
-    c = Mizur.map2(a, b, fn(x, y) -> x + y end)
-    assert Mizur.unwrap(c) == 46
-  end 
-
-  test "failure for map2" do 
-    a = Money.euro(12)
-    b = Length.dm(34)
-    assert_raise RuntimeError, "#{Length} is not compatible with #{Money}", 
-    fn ->  _ = Mizur.map2(a, b, fn(x, y) -> x + y end) end
-  end
-
-  test "for fold" do 
-    result = 
-      [Length.cm(100), Length.km(1), Length.m(13)]
-      |> Mizur.fold(
-        Length.m(0), 
-        fn(x, acc) -> 
-          Mizur.map2(x, acc, fn(a, b) -> a+b end) 
-        end,
-        to: Length.m
-        )
-      |> Mizur.unwrap
-      assert result == 1014.0
-
-  end
-
-  test "for Sum" do 
-    result = 
-      [Length.m(12), Length.km(1), Length.cm(14)]
-      |> Mizur.sum(to: Length.cm)
-      |> Mizur.unwrap()
-
-    assert result == (100000 + 1200 + 14)*1.0
-    
-  end
-
-  test "for comparison :eq 1" do 
-    x = Length.m(1)
-    y = Length.cm(100)
-    assert (Mizur.compare(x, with: y)) == :eq
-    assert (Mizur.compare(x, with: x)) == :eq
-    assert (Mizur.compare(y, with: y)) == :eq
-    assert (Mizur.compare(y, with: x)) == :eq
-  end
-
-  test "for comparison :lt/gt 1" do 
-    x = Length.m(23)
-    y = Length.cm(100)
-    assert (Mizur.compare(x, with: y)) == :gt
-    assert (Mizur.compare(y, with: x)) == :lt
-  end
-
-  test "failure of comparison" do 
-    x = Length.m(23)
-    y = Money.euro(100)
-    assert_raise RuntimeError, "#{Length} is not compatible with #{Money}", fn -> 
-      _ = Mizur.compare(y, with: x)
-    end
-  end
-
-  test "for min" do 
-    a = Length.cm(10000)
-    b = Length.m(2)
-    assert (Mizur.min(a, b)) == b
-  end
-
-  test "for max" do 
-    a = Length.cm(10000)
-    b = Length.m(2)
-    assert (Mizur.max(a, b)) == a
-  end
-
-  test "for equals" do 
-    a = Length.m(1)
-    b = Length.cm(100)
-    assert (Mizur.max(a, b)) == a
-    assert (Mizur.min(a, b)) == a
-  end
   
 end
