9697d6c21c52ab416c93b1e3d123d1dc4e78fbd4
xvw <xavier.vdw@gmail.com>
2017-04-19 02:15:45 +0200

    reborn !

--- lib/mizur.ex
+++ lib/mizur.ex
@@ -1,5 +1,6 @@
 defmodule Mizur do
 
+
   @doc false
   defmacro __using__(_opts) do 
     quote do 
@@ -10,6 +11,7 @@ defmodule Mizur do
     end
   end
 
+
   @doc false
   defmacro __before_compile__(_env) do
     quote do
@@ -19,8 +21,38 @@ defmodule Mizur do
     end
   end
 
+
+  @doc false
   defmacro define_basis(basis) do 
-    IO.inspect basis
+    quote do 
+      @basis unquote(basis)
+      @metrics [unquote(basis) | @metrics]
+
+      def unquote(basis)() do 
+        {
+          __MODULE__, 
+          unquote(basis),
+          fn(x) -> x * 1.0 end, 
+          fn(x) -> x * 1.0 end
+        }
+      end
+
+      def unquote(basis)(value) do 
+        {
+          apply(__MODULE__, unquote(basis), []),
+          value * 1.0
+        }
+      end
+      
+    end
+  end
+
+
+  @doc false
+  defmacro define_internal_type(name, expr) do 
+    quote do 
+      @metrics [unquote(name) | @metrics]
+    end
   end
 
   
@@ -36,9 +68,32 @@ defmodule Mizur do
     end
   end
 
+  @doc false
+  defmacro type({:=, _, [{name, _, nil}, rest]}) do 
+    quote do 
+      case @basis do 
+        nil -> 
+          raise RuntimeError, 
+            message: "Basis must be defined"
+        _ -> 
+          cond do 
+            Enum.member?(@metrics, unquote(name)) ->
+              raise RuntimeError, 
+                message: "#{unquote(name)} is already defined"
+            true -> define_internal_type(unquote(name), unquote(rest))
+              
+          end
+      end
+    end
+  end
+  
+
+  @doc false
   defmacro type(value) do 
-    IO.inspect value
+    raise RuntimeError, 
+      message: "The line is unparsable"
   end
 
+  def unwrap({_, value}), do: value
 
 end
--- test/mizur_test.exs
+++ test/mizur_test.exs
@@ -4,9 +4,15 @@ defmodule MizurTest do
 
   defmodule Test do 
     use Mizur 
+
     type celsius
-    #type farenheit = celsius * 1.8 + 32.0
+    type farenheit = celsius * 1.8 + 32.0
+    type cm = 18
+
   end
+
+
+  IO.inspect (Test.celsius(12) |> Mizur.unwrap)
 
   
 end
