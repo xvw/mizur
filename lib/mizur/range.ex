4dea441f812d118e46e24a0c438298eb22db35ef
xvw <xavier.vdw@gmail.com>
2017-04-26 13:40:40 +0200

    Initialize range

--- lib/mizur/range.ex
+++ lib/mizur/range.ex
@@ -15,11 +15,20 @@ defmodule Mizur.Range do
 
 
   @doc """
+  Builds a range between two `typed_value`. if the two members 
+  of the function are the same, the function will raise an 
+  `ArgumentError`.
+
+      iex> Mizur.Range.new(MizurTest.Distance.cm(1), MizurTest.Distance.cm(10))
+      {MizurTest.Distance.cm(1), MizurTest.Distance.cm(10), true}
+
+      iex> Mizur.Range.new(MizurTest.Distance.m(1), MizurTest.Distance.cm(10))
+      {MizurTest.Distance.m(1), MizurTest.Distance.cm(10), false}
   """
   @spec new(Mizur.typed_value, Mizur.typed_value) :: range 
   def new(a, b) do 
     ord =
-      case Mizur.compare(a, b) do 
+      case Mizur.compare(a, with: b) do 
         :lt -> true 
         :gt -> false 
         :eq -> 
